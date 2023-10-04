eb78adf899e21a92637b84a8e69c593ec7035fd8
Ryan Caudill <rcaudill@email.sc.edu>
2023-10-04 13:20:01 -0400

    Finished program

--- iterator/Document.java
+++ iterator/Document.java
@@ -1,7 +1,12 @@
 package iterator;
 
 import java.util.Stack;
-
+/**
+ * @author Ryan Caudill
+ * Document class holds two stacks of strings, undo and redo
+ * Undo command puts top item of undostack onto redostack
+ * Redo command puts top item of redostack back onto undostack
+ */
 public class Document {
   private String title;
   private Stack<String> undoStack = new Stack<String>();
@@ -10,25 +15,71 @@ public class Document {
   Document(String title) {
     this.title = title;
   }
+  /**
+   * Adds change to undoStack and returns String 
+   * saying making change
+   * @param change String to add to undoStack
+   * @return String: "Making change: change"
+   */
   public String makeChange(String change) {
-
+    undoStack.push(change);
+    String returnString = "Making change: " + change;
+    return returnString;
   }
+  /**
+   * First checks if a change can be undone, then
+   * moves item from undoStack to redoStack
+   * @return String: "Undoing change: change"
+   */
   public String undoChange() {
-
+    if (!canUndo()) {
+      return "Cannot undo";
+    }
+    String temp = undoStack.pop();
+    redoStack.push(temp);
+    String returnString = "Undoing change: " + temp;
+    return returnString;
   }
+  /**
+   * @return boolean if undoStack is empty
+   */
   public boolean canUndo() {
-
+    return !undoStack.empty();
   }
+  /**
+   * First checks if redoStack is empty, then
+   * moves item from redoStack to undoStack
+   * @return String: "Redoing change: change"
+   */
   public String redoChange() {
-
+    if (!canRedo()) {
+      return "Cannot redo";
+    }
+    String temp = redoStack.pop();
+    undoStack.push(temp);
+    String returnString = "Redoing change: " + temp;
+    return returnString;
   }
+  /**
+   * @return boolean is redoStack is empty or not
+   */
   public boolean canRedo() {
-
+    return !redoStack.empty();
   }
+  /**
+   * Makes StackIterator of undoStack
+   * @return undoIterator
+   */
   public StackIterator getUndoIterator() {
-
+    StackIterator undoIterator = new StackIterator(undoStack);
+    return undoIterator;
   }
+  /**
+   * Makes StackIterator of redoStack
+   * @return redoIterator
+   */
   public StackIterator getRedoIterator() {
-    
+    StackIterator redoIterator = new StackIterator(redoStack);
+    return redoIterator;
   }
 }
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -2,20 +2,36 @@ package iterator;
 
 import java.util.Stack;
 import java.util.Iterator;
-
+/**
+ * @author Ryan Caudill
+ * StackIterator class implements util interface Iterator
+ * has methods to iterate through a stack in reverse
+ */
 public class StackIterator implements Iterator<String> {
   private Stack<String> items = new Stack<String>();
   private int position;
 
   StackIterator(Stack<String> items) {
     this.items = items;
-    position = 1;
+    position = 0;
   }
+  /**
+   * next parses through stack in reverse (bottom -> top)
+   * @return next item on the stack from the bottom
+   */
   public String next() {
+    if (!hasNext()) {
+      return "";
+    }
+    String returnString = items.get(position);
     position++;
-    return items.pop();
+    return returnString;
   }
+  /**
+   * checks if position has gone all the way through the stack
+   * @return if stack has any more items
+   */
   public boolean hasNext() {
-    return !items.empty();
+    return position < items.size();
   }
 }
